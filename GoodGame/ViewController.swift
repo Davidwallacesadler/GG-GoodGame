42df26265833f9cf37451d8df6cab552e0cdfbb0
David Sadler <Davidwallacesadler@gmail.com>
2019-09-24 11:09:25 -0600

    Initial Commit

---
+++ GoodGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodGame
+//
+//  Created by David Sadler on 9/24/19.
+//  Copyright © 2019 David Sadler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
