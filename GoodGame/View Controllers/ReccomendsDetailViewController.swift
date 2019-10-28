2a31e1742b420394e468bbf8fe0aa62f457994ee
David Sadler <Davidwallacesadler@gmail.com>
2019-10-28 16:25:58 -0600

    Implement flipping of randomGameButton in RecommendationDetailVC to make it easy to go through multiple presses

--- GoodGame/View Controllers/ReccomendsDetailViewController.swift
+++ GoodGame/View Controllers/ReccomendsDetailViewController.swift
@@ -13,13 +13,13 @@ class ReccomendsDetailViewController: UIViewController {
     // MARK: - Internal Properties
     var selectedGame: SavedGame?
     lazy var slideInTransitioningDelegate = SlideInPresentationManager()
+    var randomButtonFlipped = false
     
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ViewHelper.roundCornersOf(viewLayer: randomGameButton.layer, withRoundingCoefficient: 10.0)
-        ViewHelper.roundCornersOf(viewLayer: reccomendByFilterButton.layer, withRoundingCoefficient: 10.0)
+        roundCornersOfButtons()
     }
     
     // MARK: - Outlets
@@ -33,7 +33,42 @@ class ReccomendsDetailViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     @IBAction func randomGameButtonPressed(_ sender: Any) {
-        getRandomGame()
+        if randomButtonFlipped {
+            self.performSegue(withIdentifier: "toShowGamePlayStatus", sender: self)
+        } else {
+           getRandomGameAndFlipCard()
+        }
+    }
+    
+    // MARK: - Internal Methods
+    
+    @objc func flipCardAgain(_ sender: UITapGestureRecognizer) {
+        getRandomGameAndFlipCard()
+    }
+    
+    private func getRandomGameAndFlipCard() {
+        randomButtonFlipped = true
+        guard let randomGame = SavedGameController.shared.savedGames.randomElement() else { return }
+        selectedGame = randomGame
+        let randomGameImage = randomGame.photo
+        randomGameButton.setBackgroundImage(randomGameImage, for: .normal)
+        let backArrowIconView = UIView(frame: CGRect(x: 0, y: 0, width: 50.0, height: 50.0))
+        let backArrowIconImage = UIImage(named: "backArrowIcon")
+        let backArrowIconImageView = UIImageView(image: backArrowIconImage)
+        backArrowIconImageView.frame = backArrowIconView.frame
+        backArrowIconImageView.tintColor = .goodGamePinkBright
+        backArrowIconView.addSubview(backArrowIconImageView)
+        randomGameButton.addSubview(backArrowIconView)
+        let flipBackGesture = UITapGestureRecognizer(target: self, action: #selector(flipCardAgain(_:)))
+        backArrowIconView.addGestureRecognizer(flipBackGesture)
+        randomGameButton.setImage(UIImage(), for: .normal)
+        randomGameButton.setTitle("\(randomGame.title!)", for: .normal)
+        UIView.transition(with: randomGameButton, duration: 0.5, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+    }
+    
+    private func roundCornersOfButtons() {
+        ViewHelper.roundCornersOf(viewLayer: randomGameButton.layer, withRoundingCoefficient: 10.0)
+        ViewHelper.roundCornersOf(viewLayer: reccomendByFilterButton.layer, withRoundingCoefficient: 10.0)
     }
     
     private func getRandomGame() {
@@ -41,9 +76,6 @@ class ReccomendsDetailViewController: UIViewController {
         selectedGame = randomGame
         self.performSegue(withIdentifier: "toShowGamePlayStatus", sender: self)
     }
-    
-    #warning("want to segue to the now playing VC with the random game")
-
     
     // MARK: - Navigation
 
